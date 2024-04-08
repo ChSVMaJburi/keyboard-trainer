b87168b6a983a4f68389bf75989947efd0d0e0f1
Шахром Аминов <aminov.sh@phystech.edu>
2024-04-08 22:00:41 +0300

    Added text loader and texts

---
+++ text_loader.py
@@ -0,0 +1,18 @@
+"""Файл с реализацией TextLoader"""
+import os
+import random
+
+
+class TextLoader:
+    """Класс, который загружает текст из файла"""
+
+    def __init__(self, directory_path: str) -> None:
+        self.texts_path = directory_path
+        self.count_of_texts = len(os.listdir(directory_path))
+
+    def load_random_text(self) -> str:
+        """Загружает рандомный текст из файла и возвращает его в виде строки."""
+        index = random.randint(1, self.count_of_texts)
+        with open(f"{self.texts_path}/{index}.txt", encoding="UTF-8") as text:
+            return text.read()
+
